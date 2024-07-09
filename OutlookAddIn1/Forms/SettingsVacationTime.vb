3572d04a3dbb77e95b8228f604378999bf7418aa
SmithCZE <ondrejnyklicek@icloud.com>
2024-07-09 15:18:09 +0200

    Improving settings

--- OutlookAddIn1/Forms/SettingsVacationTime.vb
+++ OutlookAddIn1/Forms/SettingsVacationTime.vb
@@ -6,8 +6,6 @@ Public Class SettingsVacationTime
     Private Property endDate As Date
     Private Property isOnVacationPlan As Boolean
 
-
-
     Private Sub SettingsVacationTime_Load(sender As Object, e As EventArgs) Handles MyBase.Load
 
         If Globals.ThisAddIn.GetStartDate <> Date.MinValue Or Not Globals.ThisAddIn.GetStartDate < Date.Today Then
@@ -22,16 +20,11 @@ Public Class SettingsVacationTime
             DateTimePickerEnd.Value = Date.Today.AddDays(7)
         End If
 
-        VacationPlanCheck.Checked = Globals.ThisAddIn.GetisVacationPlanActive
-
+        VacationPlanCheck.Checked = Globals.ThisAddIn.GetIsVacationPlanActive
     End Sub
 
-    Private Sub Save_Click(sender As Object, e As EventArgs) Handles btnSave.Click
-        If Not SaveData() Then
-            Exit Sub
-        End If
-
-        Close()
+    Private Sub VacationPlanCheck_CheckedChanged(sender As Object, e As EventArgs) Handles VacationPlanCheck.CheckedChanged
+        isOnVacationPlan = VacationPlanCheck.Checked
     End Sub
 
     Private Sub DateTimePicker1_ValueChanged(sender As Object, e As EventArgs) Handles DateTimePickerStart.ValueChanged
@@ -42,6 +35,14 @@ Public Class SettingsVacationTime
         endDate = DateTimePickerEnd.Value
     End Sub
 
+    Private Sub Save_Click(sender As Object, e As EventArgs) Handles btnSave.Click
+        If Not SaveData() Then
+            Exit Sub
+        End If
+
+        Close()
+    End Sub
+
     Private Function SaveData() As Boolean
 
         'Validate data
@@ -49,27 +50,27 @@ Public Class SettingsVacationTime
             Return False
         End If
 
-        Globals.ThisAddIn.ToggleVacationPlanResonder(True)
         Globals.ThisAddIn.TimeVacationResponder(startDate, endDate)
-        Globals.ThisAddIn.ToggleVacationPlanResonder(VacationPlanCheck.Checked)
+        Globals.ThisAddIn.ToggleVacationPlanResonder(isOnVacationPlan)
+
+        If (isOnVacationPlan) Then
+            Message.HasSetDate(startDate, endDate)
+        Else
+            Message.HasActiveByPlan(isOnVacationPlan)
+        End If
 
         Return True
 
     End Function
 
-    Private Sub VacationPlanCheck_CheckedChanged(sender As Object, e As EventArgs) Handles VacationPlanCheck.CheckedChanged
-        isOnVacationPlan = VacationPlanCheck.Checked
-    End Sub
 
     Private Overloads Function Validate() As Boolean
         'Validate start date
         If startDate < Date.MinValue Or
             startDate < Date.Today Then
             MessageBox.Show($"Nelze nastavit datum začátku v minulosti.", "Naplatné datum", MessageBoxButtons.OK, MessageBoxIcon.Error)
-
             DateTimePickerStart.Value = Date.Today
             Return False
-
         End If
 
         'Validate end date
@@ -79,10 +80,10 @@ Public Class SettingsVacationTime
         ElseIf DateTimePickerEnd.Value < DateTimePickerStart.Value Then
             MessageBox.Show($"Nelze nastavit datum před začátkem dovolené.", "Naplatné datum", MessageBoxButtons.OK, MessageBoxIcon.Error)
             DateTimePickerEnd.Value = Date.Today.AddDays(7)
-
             Return False
         End If
 
         Return True
+
     End Function
 End Class
